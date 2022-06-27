b0c68a2d05b7452de1efb205baf88878609a81a5
KalebNotCaleb <kalebkroener@gmail.com>
2022-06-27 18:25:34 -0500

    Debug if/else statements

--- script.js
+++ script.js
@@ -1,10 +1,45 @@
-const computerOptions = ["Rock", "Paper", "Scissors"];
+const computerArray = ["Rock", "Paper", "Scissors"];
 
-let computerChoice = Math.floor(Math.random() * computerOptions.length);
+let computerNum = Math.floor(Math.random() * computerArray.length);
+
+let computerChoice = function(computerNum) {
+    if (computerNum < 1) {
+        return "Rock";
+    } else if (computerNum > 1) {
+        return "Scissors";
+    } else {
+        return "Paper";
+    }
+}
+
+console.log(computerChoice(computerNum))
+
+let str = 'pApER'
 
 let playerChoice = function(str) {
     return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 }
 
+console.log(playerChoice(str))
+
+function playRound (playerChoice, computerChoice) {
+    if (playerChoice === "Rock" && computerChoice === "Scissors") {
+        console.log("You win! Rock beats Scissors.");
+    } else if (playerChoice === "Rock" && computerChoice === "Paper") {
+        console.log("You lose! Rock loses to Paper.");
+    } else if (playerChoice === "Paper" && computerChoice === "Rock") {
+        console.log("You win! Paper beats Rock!");
+    } else if (playerChoice === "Paper" && computerChoice === "Scissors") {
+        console.log("You lose! Paper loses to Scissors.");
+    } else if (playerChoice === "Scissors" && computerChoice === "Paper") {
+        console.log("You win! Scissors beat Paper!");
+    } else if (playerChoice === "Scissors" && computerChoice === "Rock") {
+        console.log("You lose! Scissors lose to Rock.");
+    } else {
+        console.log("You tied. Try again.")
+    }       
+}
+
+console.log(playRound(playerChoice, computerChoice))
 
 
