c36f540d4ad2a74384a6c0e7e08a9d6bf42aa36a
KalebNotCaleb <kalebkroener@gmail.com>
2022-06-30 15:34:31 -0500

    Add logic to display computer choice

--- script.js
+++ script.js
@@ -2,12 +2,25 @@ let playPrompt = prompt("Pick your weapon: rock, paper, or scissors?");
 
 let playerChoice = playPrompt.charAt(0).toUpperCase() + playPrompt.slice(1).toLowerCase();
 
-let computerChoice = Math.floor(Math.random() * 3);
+let computerNum = Math.floor(Math.random() * 3);
+
+function compLogic(computerNum) {
+    if (computerNum === 0) {
+        return "Rock";
+    } else if (computerNum === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
+}
+
+let computerChoice = compLogic(computerNum);
+
 
 function playRound(playerChoice, computerChoice) {
-    if (playerChoice === "Rock" && computerChoice === 2 || playerChoice === "Paper" && computerChoice === 0 || playerChoice === "Scissors" && computerChoice === 1) {
+    if (playerChoice === "Rock" && computerChoice === "Scissors" || playerChoice === "Paper" && computerChoice === "Rock" || playerChoice === "Scissors" && computerChoice === "Paper") {
         return `You win! ${playerChoice} beats ${computerChoice}!`;
-    } else if (playerChoice === "Rock" && computerChoice === 1 || playerChoice === "Paper" && computerChoice === 2 || playerChoice === "Scissors" && computerChoice === 0) {
+    } else if (playerChoice === "Rock" && computerChoice === "Paper" || playerChoice === "Paper" && computerChoice === "Scissors" || playerChoice === "Scissors" && computerChoice === "Rock") {
         return `You lose! ${playerChoice} loses to ${computerChoice}!`;
     } else {
         return "It's a tie!";
