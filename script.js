de611740aca845327087211aac05ce4d6a68ac5c
KalebNotCaleb <kalebkroener@gmail.com>
2022-06-30 19:04:44 -0500

    Add looping logic

--- script.js
+++ script.js
@@ -1,31 +1,45 @@
-let playPrompt = prompt("Pick your weapon: rock, paper, or scissors?");
+let playerScore = parseInt(0);
+let computerScore = parseInt(0);
 
-let playerChoice = playPrompt.charAt(0).toUpperCase() + playPrompt.slice(1).toLowerCase();
+for(let i=0; i<100; i++) {
+    let playPrompt = prompt("Pick your weapon: rock, paper, or scissors?");
 
-let computerNum = Math.floor(Math.random() * 3);
+    let playerChoice = playPrompt.charAt(0).toUpperCase() + playPrompt.slice(1).toLowerCase();
 
-function compLogic(computerNum) {
-    if (computerNum === 0) {
-        return "Rock";
-    } else if (computerNum === 1) {
-        return "Paper";
-    } else {
-        return "Scissors";
+    let computerNum = Math.floor(Math.random() * 3);
+
+    function compLogic(computerNum) {
+        if (computerNum === 0) {
+            return "Rock";
+        } else if (computerNum === 1) {
+            return "Paper";
+        } else {
+            return "Scissors";
+        }
+    }
+
+    let computerChoice = compLogic(computerNum);
+
+    function playRound(playerChoice, computerChoice) {
+        if (playerChoice === "Rock" && computerChoice === "Scissors" || playerChoice === "Paper" && computerChoice === "Rock" || playerChoice === "Scissors" && computerChoice === "Paper") {
+            playerScore++;
+            return `You win! ${playerChoice} beats ${computerChoice}!`;
+        } else if (playerChoice === "Rock" && computerChoice === "Paper" || playerChoice === "Paper" && computerChoice === "Scissors" || playerChoice === "Scissors" && computerChoice === "Rock") {
+            computerScore++;
+            return `You lose! ${playerChoice} loses to ${computerChoice}!`;
+        } else {
+            return "It's a tie!";
+        }
+    }
+
+   
+    console.log(playRound(playerChoice, computerChoice));
+    console.log(`Your score is ${playerScore}!`);
+    console.log(`The computer's score is ${computerScore}!`);
+
+    if (computerScore === 5 || playerScore === 5) {
+        break;
     }
 }
 
-let computerChoice = compLogic(computerNum);
-
-
-function playRound(playerChoice, computerChoice) {
-    if (playerChoice === "Rock" && computerChoice === "Scissors" || playerChoice === "Paper" && computerChoice === "Rock" || playerChoice === "Scissors" && computerChoice === "Paper") {
-        return `You win! ${playerChoice} beats ${computerChoice}!`;
-    } else if (playerChoice === "Rock" && computerChoice === "Paper" || playerChoice === "Paper" && computerChoice === "Scissors" || playerChoice === "Scissors" && computerChoice === "Rock") {
-        return `You lose! ${playerChoice} loses to ${computerChoice}!`;
-    } else {
-        return "It's a tie!";
-    }
-}
-
-console.log(playRound(playerChoice, computerChoice))
 
